64f92211fec4341b84b25a431a3143ca31546823
Zakaria Nadir <zacanadir@gmail.com>
2025-08-18 15:30:44 -0400

    Update logic in main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Cloud Build CI/CD!"
+    return "Hello there, This is CNN, the world's news leader lol"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
